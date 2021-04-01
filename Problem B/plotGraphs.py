081d980b793a247fc6a89d17ac9d808b1e96eb50
Arwin Saleh <arwinsaleh@hotmail.com>
2021-04-01 21:49:59 +0200

    fixed figuresd

--- Problem B/plotGraphs.py
+++ Problem B/plotGraphs.py
@@ -1,8 +1,13 @@
+from datetime import time
 from matplotlib import pyplot as plt
 import numpy as np
 from numpy.core.fromnumeric import size
 import pandas as pd
-from scipy.signal import lfilter
+import operator
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
 
 
 def task1a():
@@ -11,10 +16,30 @@ def task1a():
     n_episodes = len(rewards)
     episodes = np.linspace(0,n_episodes, n_episodes)
 
-    plt.plot(episodes, rewards)
+    plt.plot(episodes, rewards, label="Rewards")
+
+    rewards = np.array(rewards).reshape((len(rewards)))
+
+    episodes = episodes[:, np.newaxis]
+    rewards = rewards[:, np.newaxis]
+
+    poly_feat = PolynomialFeatures(degree=6)
+    ep_poly = poly_feat.fit_transform(episodes)
+
+    model = LinearRegression()
+    model.fit(ep_poly, rewards)
+
+    reward_poly_pred = model.predict(ep_poly)
+
+    # sort the values of x before line plot
+    sort_axis = operator.itemgetter(0)
+    sorted_zip = sorted(zip(episodes,reward_poly_pred), key=sort_axis)
+    episodes, reward_poly_pred = zip(*sorted_zip)
+    plt.plot(episodes, reward_poly_pred, color='r', label="Fit")
 
     plt.xlabel('E', size=24)
     plt.ylabel('R', size=24)
+    plt.legend()
     plt.show()
 
 def task1b():
@@ -23,10 +48,30 @@ def task1b():
     n_episodes = len(rewards)
     episodes = np.linspace(0,n_episodes, n_episodes)
 
-    plt.plot(episodes, rewards)
-    
+    plt.plot(episodes, rewards, label="Rewards")
+
+    rewards = np.array(rewards).reshape((len(rewards)))
+
+    episodes = episodes[:, np.newaxis]
+    rewards = rewards[:, np.newaxis]
+
+    poly_feat = PolynomialFeatures(degree=4)
+    ep_poly = poly_feat.fit_transform(episodes)
+
+    model = LinearRegression()
+    model.fit(ep_poly, rewards)
+
+    reward_poly_pred = model.predict(ep_poly)
+
+    # sort the values of x before line plot
+    sort_axis = operator.itemgetter(0)
+    sorted_zip = sorted(zip(episodes,reward_poly_pred), key=sort_axis)
+    episodes, reward_poly_pred = zip(*sorted_zip)
+    plt.plot(episodes, reward_poly_pred, color='r', label="Fit")
+
     plt.xlabel('E', size=24)
     plt.ylabel('R', size=24)
+    plt.legend()
     plt.show()
 
 def task1c():
@@ -35,23 +80,32 @@ def task1c():
     n_episodes = len(rewards)
     episodes = np.linspace(0,n_episodes, n_episodes)
 
-    plt.plot(episodes, rewards)
+    plt.plot(episodes, rewards, label="Rewards")
+
+    rewards = np.array(rewards).reshape((len(rewards)))
+
+    episodes = episodes[:, np.newaxis]
+    rewards = rewards[:, np.newaxis]
+
+    poly_feat = PolynomialFeatures(degree=3)
+    ep_poly = poly_feat.fit_transform(episodes)
+
+    model = LinearRegression()
+    model.fit(ep_poly, rewards)
+
+    reward_poly_pred = model.predict(ep_poly)
+
+    # sort the values of x before line plot
+    sort_axis = operator.itemgetter(0)
+    sorted_zip = sorted(zip(episodes,reward_poly_pred), key=sort_axis)
+    episodes, reward_poly_pred = zip(*sorted_zip)
+    plt.plot(episodes, reward_poly_pred, color='r', label="Fit")
+
     plt.xlabel('E', size=24)
     plt.ylabel('R', size=24)
-    plt.show()
-
-def task1d():
-
-    rewards = pd.read_csv("Problem B\\Task 1\\1c.csv")
-    n_episodes = len(rewards)
-    episodes = np.linspace(0,n_episodes, n_episodes)
-
-    plt.plot(episodes, rewards)
-    plt.xlabel('E', size=24)
-    plt.ylabel('R', size=24)
+    plt.legend()
     plt.show()
 
 task1a()
 #task1b()
 #task1c()
-#task1d()
